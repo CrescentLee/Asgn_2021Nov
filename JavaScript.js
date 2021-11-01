290102d6f105e7e338d8b7c29416734afe41fac6
CrescentLee <lmz0410@hotmail.com>
2021-11-02 00:38:22 +0800

    Add files

---
+++ JavaScript.js
@@ -0,0 +1,24 @@
+﻿//init element when DOM is loaded
+$(window).ready(function () {
+    togglebuttoncheck();
+})
+
+//resize event
+$(window).resize(function () {
+    togglebuttoncheck();
+});
+
+//toggle function for button
+function triggerToggle() {
+    $('#nav_bar').slideToggle("slow", function () {
+    });
+}
+
+//check width to show/hide the nav bar
+function togglebuttoncheck() {
+    if ($(window).width() <= 540) {
+        $('#nav_bar').css('display', 'none');
+    } else {
+        $('#nav_bar').css('display', 'block');
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+    return gulp.src('bootstrap_scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+};
+
+exports.default = buildStyles;
+exports.watch = function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+};
